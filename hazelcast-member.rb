149c16dfb065a0098def41c37a5e534d9667ba1b
Guglielmo Nigri <googlielmo@gmail.com>
2018-03-16 10:33:55 +0100

    Version 3.9.3 patch 3
     - fix audit warnings in the formula

--- hazelcast-member.rb
+++ hazelcast-member.rb
@@ -1,14 +1,13 @@
 class HazelcastMember < Formula
-  desc "A command line tool for running one or more Hazelcast member instance(s) on the local machine"
-  homepage "https://github.com/hazelcast/hazelcast-member-tool"
-  version "3.9.3_2"
-  url "https://github.com/hazelcast/hazelcast-member-tool/raw/master/archive/hazelcast-member-#{version}.tar.gz"
-  sha256 "518e942c21aa7effbc117a4dab5d0fbe9f8c864a3d17389e577947aa04777d89"
+  desc "Command-line tool for running Hazelcast member instance(s)"
+  homepage "https://github.com/hazelcast/hazelcast"
+  url "https://github.com/hazelcast/hazelcast-member-tool/raw/master/archive/hazelcast-member-3.9.3.3.tar.gz"
+  sha256 "cbbe4d175b473b443eb3aeb0fbdd297c9e784db73ee2f2a409a9c6180c5c4157"
+
+  bottle :unneeded
 
   depends_on "coreutils"
   depends_on :java => "1.7+"
-
-  bottle :unneeded
 
   def install
     mkpath "#{etc}/hazelcast"
@@ -16,16 +15,32 @@ class HazelcastMember < Formula
     etc.install_symlink "#{libexec}/etc/hazelcast.xml" => "hazelcast/hazelcast.xml"
     bin.install_symlink Dir["#{libexec}/bin/hazelcast-member"]
     var.mkpath
-    inreplace "#{libexec}/bin/utils.sh", "${var}", "#{var}"
-    inreplace "#{libexec}/bin/utils.sh", "${etc}", "#{etc}"
+    inreplace "#{libexec}/bin/utils.sh", "${var}", var.to_s
+    inreplace "#{libexec}/bin/utils.sh", "${etc}", etc.to_s
     prefix.install_metafiles
     bash_completion.install "#{libexec}/bin/hazelcast-member-completion.sh" => "hazelcast-member"
   end
 
   def caveats
-    <<-EOS
-To enable tab completion add to your profile:
-  if which hazelcast-member > /dev/null; then eval "$(hazelcast-member init -)"; fi
+    <<~EOS
+      To enable tab completion add to your profile:
+        if which hazelcast-member > /dev/null; then eval "$(hazelcast-member init -)"; fi
     EOS
+  end
+
+  test do
+    begin
+      output = shell_output("#{bin}/hazelcast-member start --verbose -J -Dhazelcast.http.healthcheck.enabled=true")
+      assert_match /ID:/, output
+      sleep 3
+      output = shell_output("#{bin}/hazelcast-member status")
+      assert_match /Running/, output
+
+      assert_match /Hazelcast::NodeState=ACTIVE/, shell_output("curl -s http://127.0.0.1:5701/hazelcast/health")
+      output = shell_output("#{bin}/hazelcast-member stop")
+      assert_match /shut down/, output
+    ensure
+      quiet_system "pkill", "-9", "-f", "hazelcast"
+    end
   end
 end
