7c9465905b063f7535805cf04752db2c33a2b1ab
alparslanavci <alparslanavci@gmail.com>
2020-08-27 13:43:24 +0100

    Updated with the latest Hazelcast Command Line release

--- hazelcast.rb
+++ hazelcast.rb
@@ -1,17 +1,16 @@
 class Hazelcast < Formula
   desc "Tool to run Hazelcast IMDG member instances locally"
   homepage "https://github.com/hazelcast/hazelcast-command-line"
-  url "https://github.com/hazelcast/hazelcast-command-line/releases/download/v0.2-beta/hazelcast-command-line-0.2-beta.tar.gz"
-  sha256 "04bd3768769e81d60adaf456361690668ed3e71b221a0224bb398ed9bd2eb93f"
+  url "https://github.com/hazelcast/hazelcast-command-line/releases/download/v4.2020.08.1/hazelcast-command-line-4.2020.08.1.tar.gz"
+  sha256 "65d95a51fcdb108a90af750b601e9bb8a3ad8fa308ef54cb2536eae64bd73b3f"
 
   bottle :unneeded
 
-  depends_on "coreutils"
   depends_on :java => "1.8+"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink Dir["#{libexec}/bin/hazelcast"]
+    bin.install_symlink Dir["#{libexec}/bin/hz"]
     prefix.install_metafiles
   end
 
