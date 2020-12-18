16cb6ade161f3c4fb605708ac63a3757b3fa53f0
runner <runner@Mac-1608300628030.local>
2020-12-18 14:13:47 +0000

    hzcloud 1.3.1

--- hzcloud.rb
+++ hzcloud.rb
@@ -1,8 +1,8 @@
 class Hzcloud < Formula
   desc "Hazelcast Cloud Enterprise is an enterprise-grade, on-demand managed service for Hazelcast IMDG"
   homepage "http://cloud.hazelcast.com/"
-  url "https://github.com/hazelcast/hazelcast-cloud-cli/archive/v1.3.0.tar.gz"
-  sha256 "8bf98f2e6643714d097a686903a7abe7e11dff19291c205de33eee7d681e88ca"
+  url "https://github.com/hazelcast/hazelcast-cloud-cli/archive/v1.3.1.tar.gz"
+  sha256 "c4c91713f8826fd94b79b7b7dcc0a6db00379db7d4f8cfba5ed1e9aaac12ec63"
   depends_on "go" => :build
   
   def install
