6821c20b4e05b3c17e4b3c4442402560f2ea7455
devOpsHazelcast <devops@hazelcast.com>
2020-12-22 11:16:04 +0000

    Hazelcast Command Line 4.2020.12 release

---
+++ hazelcast@4.2020.12.rb
@@ -0,0 +1,22 @@
+class HazelcastAT4202012 < Formula
+  desc "Tool to run Hazelcast IMDG member instances locally"
+  homepage "https://github.com/hazelcast/hazelcast-command-line"
+  url "https://github.com/hazelcast/hazelcast-command-line/releases/download/v4.2020.12/hazelcast-4.1.1.tar.gz"
+  sha256 "d371df00e9fffdd32f04bbdb192b9b669b6dd30cb043b0d9c2029fc9c93c353c"
+
+  bottle :unneeded
+
+  depends_on "openjdk" => :recommended
+
+  def install
+    libexec.install Dir["*"]
+    (bin/"hz").write_env_script libexec/"bin/hz", Language::Java.overridable_java_home_env
+    prefix.install_metafiles
+    inreplace libexec/"bin/download/hazelcast-download.properties", "hazelcastDownloadId=CLI", "hazelcastDownloadId=CLI_BREW"
+  end
+
+  def post_install
+    exec "hz"
+  end
+
+end
