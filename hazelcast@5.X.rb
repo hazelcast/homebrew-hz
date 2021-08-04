b00aba89142c19e513032dd6893fa1e8748a8d7d
Farukhzhon Barotov <41333128+fbarotov@users.noreply.github.com>
2021-08-04 11:37:31 +0300

    hazelcast@5.X.rb, add hz-cli to path (#16)
    * hazelcast@5.X.rb, add hz-cli to path
    
    * export all commands

---
+++ hazelcast@5.X.rb
@@ -0,0 +1,29 @@
+class HazelcastAT5X < Formula
+    desc "Tool to run Hazelcast IMDG member instances locally"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://github.com/hazelcast/hazelcast-command-line/releases/download/v5.2021.07.1/hazelcast-5.0-BETA-1.tar.gz"
+    sha256 "f108d22a1aec61bbd637f89ff522af7d9861ef13afcfad24a5095127f04f091d"
+  
+    bottle :unneeded
+  
+    depends_on "openjdk" => :recommended
+  
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+      inreplace libexec/"bin/download/hazelcast-download.properties", "hazelcastDownloadId=CLI", "hazelcastDownloadId=CLI_BREW"
+    end
+  
+    def post_install
+      exec "hz"
+    end
+  
+  end
+  
