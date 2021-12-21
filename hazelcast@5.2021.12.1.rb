a45881585e5e9074cabf2b930684e27bffe6c8f9
devOpsHazelcast <devops@hazelcast.com>
2021-12-21 10:10:30 +0000

    Hazelcast Command Line 5.2021.12.1 release

---
+++ hazelcast@5.2021.12.1.rb
@@ -0,0 +1,29 @@
+class HazelcastAT52021121 < Formula
+    desc "Tool to run Hazelcast IMDG member instances locally"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://github.com/hazelcast/hazelcast-command-line/releases/download/v5.2021.12.1/hazelcast-5.0.2.tar.gz"
+    sha256 "53aa09fcf288a5d88d10251f119848b9139356ac21e94939331aa6d292893349"
+  
+    bottle :unneeded
+  
+    depends_on "openjdk" => :recommended
+  
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+      inreplace libexec/"bin/download/hazelcast-download.properties", "hazelcastDownloadId=CLI", "hazelcastDownloadId=CLI_BREW"
+    end
+  
+    def post_install
+      exec "hz"
+    end
+  
+  end
+  
