9e5d5187c15fb2627e2fb6c249bfae39ebaa0615
Frantisek Hartman <frant.hartm@gmail.com>
2022-01-18 14:43:13 +0100

    Add template for hazelcast-management-center package

---
+++ hazelcast-management-center@5.X.rb
@@ -0,0 +1,25 @@
+class HazelcastManagementCenterAT5X < Formula
+    desc "Tool to run Hazelcast Management Center"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-5.0.4.tar.gz"
+    sha256 "80828a15df806ace514174836ad5794e1bc257b171b85d582aa6f91e2acd22aa"
+
+    depends_on "openjdk" => :recommended
+
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+    end
+  
+    def post_install
+      exec "TODO"
+    end
+
+  end
