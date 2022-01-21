525b5b03a16bf033197f9236608079063831f47d
devOpsHazelcast <devops@hazelcast.com>
2022-01-21 12:03:09 +0000

    Hazelcast Homebrew Package 5.0.1 release

---
+++ hazelcast-enterprise@5.0.1.rb
@@ -0,0 +1,27 @@
+class HazelcastEnterpriseAT501 < Formula
+    desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://repository.hazelcast.com/release/com/hazelcast/hazelcast-enterprise-distribution/5.0.1/hazelcast-enterprise-distribution-5.0.1.tar.gz"
+    sha256 "a2025f1ded0f46b7fd6a413f5dc0833d954076e68e15b0b95d2fd2c7bc191424"
+    conflicts_with "hazelcast"
+  
+    depends_on "openjdk" => :recommended
+
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+      inreplace libexec/"lib/hazelcast-download.properties", "hazelcastDownloadId=distribution", "hazelcastDownloadId=brew"
+    end
+  
+    def post_install
+      exec "echo Hazelcast has been installed."
+    end
+
+  end
