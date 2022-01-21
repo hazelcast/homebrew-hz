b12d48c6984288006ceb35b81a9f26fd13d761d7
devOpsHazelcast <devops@hazelcast.com>
2022-01-21 11:13:10 +0000

    Hazelcast Homebrew Package 5.1-SNAPSHOT release

---
+++ hazelcast@5.1.snapshot.rb
@@ -0,0 +1,27 @@
+class HazelcastAT51Snapshot < Formula
+    desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.1-SNAPSHOT/hazelcast-distribution-5.1-20220121.085720-254.tar.gz"
+    sha256 "ecb1c13595b4fa5817559e7da1c2c4b57b2c8a180b8eca8be358e52aba50fc2a"
+    conflicts_with "hazelcast-enterprise"
+  
+    depends_on "openjdk" => :recommended
+
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+      inreplace libexec/"lib/hazelcast-download.properties", "hazelcastDownloadId=distribution", "hazelcastDownloadId=brew"
+    end
+  
+    def post_install
+      exec "hz"
+    end
+
+  end
