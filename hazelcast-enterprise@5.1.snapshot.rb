6355d0dcd2ff97cf0656aee0486b72a9970c7ef3
devOpsHazelcast <devops@hazelcast.com>
2022-02-18 11:49:58 +0000

    Hazelcast Homebrew Package 5.1-SNAPSHOT release

--- hazelcast-enterprise@5.1.snapshot.rb
+++ hazelcast-enterprise@5.1.snapshot.rb
@@ -1,8 +1,8 @@
 class HazelcastEnterpriseAT51Snapshot < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
-    url "https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/5.1-SNAPSHOT/hazelcast-enterprise-distribution-5.1-20220218.112507-519.tar.gz"
-    sha256 "e3189218cf8ba2a4e68e5ea101992f207e9209ee61f98580e0c90e33cc7480da"
+    url "https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/5.1-SNAPSHOT/hazelcast-enterprise-distribution-5.1-20220218.113820-520.tar.gz"
+    sha256 "ccff2dd4c5b5d45da0f8fcf3ddaf89ea0a0541554717c2ad93ba50b087eeff85"
     conflicts_with "hazelcast"
   
     depends_on "openjdk" => :recommended
