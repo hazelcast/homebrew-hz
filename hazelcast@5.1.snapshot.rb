846c3519f5da808cb73a997b2206587bfb216941
devOpsHazelcast <devops@hazelcast.com>
2022-02-22 11:08:17 +0000

    Hazelcast Homebrew Package 5.1-SNAPSHOT release

--- hazelcast@5.1.snapshot.rb
+++ hazelcast@5.1.snapshot.rb
@@ -1,8 +1,8 @@
 class HazelcastAT51Snapshot < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
-    url "https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.1-SNAPSHOT/hazelcast-distribution-5.1-20220218.130430-411.tar.gz"
-    sha256 "099ba3dcc318c628dc5ff36792925c9396206a220a91db2068e0de14fc9be3ca"
+    url "https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.1-SNAPSHOT/hazelcast-distribution-5.1-20220221.105811-413.tar.gz"
+    sha256 "21e38b5f320387ad8e34c44c55d5ee607edea35d3c47864fb3bfb10a07edecf4"
     conflicts_with "hazelcast-enterprise"
   
     depends_on "openjdk" => :recommended
@@ -16,8 +16,17 @@ class HazelcastAT51Snapshot < Formula
         end
         (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
       end
+      etc.install "#{libexec}/config" => "hazelcast"
+      rm_rf libexec/"config"
+      libexec.install_symlink "#{etc}/hazelcast" => "config"
       prefix.install_metafiles
       inreplace libexec/"lib/hazelcast-download.properties", "hazelcastDownloadId=distribution", "hazelcastDownloadId=brew"
+    end
+
+    def caveats
+        <<~EOS
+          Configuration files have been placed in #{etc}/hazelcast.
+        EOS
     end
   
     def post_install
