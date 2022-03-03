b397eece469daf1ae3defdcc915a834281f70e10
devOpsHazelcast <devops@hazelcast.com>
2022-03-03 08:06:13 +0000

    Hazelcast Homebrew Package 5.2-SNAPSHOT release

---
+++ hazelcast-snapshot.rb
@@ -0,0 +1,36 @@
+class HazelcastSnapshot < Formula
+    desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    url "https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.2-SNAPSHOT/hazelcast-distribution-5.2-20220302.124209-16.tar.gz"
+    sha256 "556366f4987c99998f8a96df5e2bf9645a7ecfbca64ecab1b9e77ded48e49fe5"
+    conflicts_with "hazelcast-enterprise"
+  
+    depends_on "openjdk" => :recommended
+
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/hz*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      etc.install "#{libexec}/config" => "hazelcast"
+      rm_rf libexec/"config"
+      libexec.install_symlink "#{etc}/hazelcast" => "config"
+      prefix.install_metafiles
+      inreplace libexec/"lib/hazelcast-download.properties", "hazelcastDownloadId=distribution", "hazelcastDownloadId=brew"
+    end
+
+    def caveats
+        <<~EOS
+          Configuration files have been placed in #{etc}/hazelcast.
+        EOS
+      end
+  
+    def post_install
+      exec "echo Hazelcast has been installed."
+    end
+
+  end
