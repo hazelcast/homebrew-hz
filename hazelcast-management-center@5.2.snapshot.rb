f2e80f6371a325a5a10cc3f12a562cec4feefc04
devOpsHazelcast <devops@hazelcast.com>
2022-03-04 10:07:41 +0000

    Hazelcast Management Center 5.2-SNAPSHOT release

---
+++ hazelcast-management-center@5.2.snapshot.rb
@@ -0,0 +1,26 @@
+class HazelcastManagementCenterAT52Snapshot < Formula
+    desc "Tool to run Hazelcast Management Center"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    version "5.0.4"
+    url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
+    sha256 "0f183e60e3829c31b27a12cc5115c770ad68b34cd8f55e877783cd49cc625192"
+
+    depends_on "openjdk" => :recommended
+
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/*mc*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+    end
+
+    def post_install
+      exec "echo Hazelcast Management Center has been installed."
+    end
+
+  end
