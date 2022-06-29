62edb1b9ec357df71b958a10390f3bd32e5a8ce8
devOpsHazelcast <devops@hazelcast.com>
2022-06-29 06:10:26 +0000

    Hazelcast Management Center 5.2-SNAPSHOT release

--- hazelcast-management-center@5.2.snapshot.rb
+++ hazelcast-management-center@5.2.snapshot.rb
@@ -3,7 +3,7 @@ class HazelcastManagementCenterAT52Snapshot < Formula
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "5.0.4"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
-    sha256 "896587bb35f80b1d9f4fe8791106c814f7bb12f31faa95fbf0ab0be0dc16b8c4"
+    sha256 "e22a0d027e4696788cde3444420f66be3eb837b2b8ee084fc46cd7c5081fb183"
 
     depends_on "openjdk" => :recommended
 
