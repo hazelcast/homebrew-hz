13db5d893c6ac914a6bc4f731d02ae9736648122
devOpsHazelcast <devops@hazelcast.com>
2022-07-05 15:01:02 +0000

    Hazelcast Management Center 5.1.4 release

--- hazelcast-management-center@5.1.4.rb
+++ hazelcast-management-center@5.1.4.rb
@@ -1,16 +1,16 @@
-class HazelcastManagementCenter < Formula
+class HazelcastManagementCenterAT514 < Formula
     desc "Tool to run Hazelcast Management Center"
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "5.1.4"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-5.1.4.tar.gz"
     sha256 "078f518074d118fc2708cbb3bf5891849730a955eef09792fa522a30e8c46fb5"
     conflicts_with "hazelcast-management-center@5.2.snapshot", because: "you can install only a single hazelcast-management-center package"
-    conflicts_with "hazelcast-management-center@5.1.4", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.1.1", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.0.4", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-5.1", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-5.0", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center", because: "you can install only a single hazelcast-management-center package"
 
     depends_on "openjdk" => :recommended
 
