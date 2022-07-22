fd26429cf5367a6d140b9b16dc7747696b99ed89
devOpsHazelcast <devops@hazelcast.com>
2022-07-22 18:37:45 +0000

    Hazelcast Management Center 5.2-SNAPSHOT release

--- hazelcast-management-center-snapshot.rb
+++ hazelcast-management-center-snapshot.rb
@@ -3,7 +3,7 @@ class HazelcastManagementCenterSnapshot < Formula
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "5.2-SNAPSHOT"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
-    sha256 "98085751967edadee5089e435a25cc3a793dd8a1c8bfe22afc26588f4b4b313a"
+    sha256 "14836c9dfa1d2319d13a4686e9a92e023f246aa7dc3a44e5aface2e5662b6ce8"
     conflicts_with "hazelcast-management-center@5.2.snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.1.4", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.1.1", because: "you can install only a single hazelcast-management-center package"
