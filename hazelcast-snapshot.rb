ea873a05f30a01539eaf9c935e9a0fb3e3ba8e78
devOpsHazelcast <devops@hazelcast.com>
2022-12-02 03:19:47 +0000

    Hazelcast Homebrew Package 5.3.0-SNAPSHOT release

--- hazelcast-snapshot.rb
+++ hazelcast-snapshot.rb
@@ -1,8 +1,8 @@
 class HazelcastSnapshot < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
-    url "https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.3.0-SNAPSHOT/hazelcast-distribution-5.3.0-20221130.140613-143.tar.gz"
-    sha256 "a49fa454ea80d3fdfab1b3a8d1997e94fe602c50a13b3df60826a1c974df6d3f"
+    url "https://oss.sonatype.org/content/repositories/snapshots/com/hazelcast/hazelcast-distribution/5.3.0-SNAPSHOT/hazelcast-distribution-5.3.0-20221201.192154-150.tar.gz"
+    sha256 "8338320451f5768d51b10ce99cee7365008e44ca71d60207db30fd3ed3bb334a"
     conflicts_with "hazelcast-enterprise@5.3.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.2.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.2.1", because: "you can install only a single hazelcast or hazelcast-enterprise package"
