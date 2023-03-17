2fc6dd16022fc9027d9b516b68050bdeb5628eef
devOpsHazelcast <devops@hazelcast.com>
2023-03-17 07:26:31 +0000

    Hazelcast Homebrew Package 5.3.0-BETA-1 release

--- hazelcast-enterprise@5.3.0.beta.1.rb
+++ hazelcast-enterprise@5.3.0.beta.1.rb
@@ -1,4 +1,4 @@
-class HazelcastEnterpriseBeta < Formula
+class HazelcastEnterpriseAT530Beta1 < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     url "https://repository.hazelcast.com/release/com/hazelcast/hazelcast-enterprise-distribution/5.3.0-BETA-1/hazelcast-enterprise-distribution-5.3.0-BETA-1.tar.gz"
