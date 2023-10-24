d218ba2080e9651165f00a01ca097d54f94c90f8
devOpsHazelcast <devops@hazelcast.com>
2023-10-24 16:05:34 +0000

    Hazelcast Homebrew Package 5.3.5 release

--- hazelcast.rb
+++ hazelcast.rb
@@ -4,6 +4,7 @@ class Hazelcast < Formula
     url "https://repo.maven.apache.org/maven2/com/hazelcast/hazelcast-distribution/5.3.5/hazelcast-distribution-5.3.5.tar.gz"
     sha256 "a78eb6e9a21e079c5798865b6521399312693533b57e099a7b6fd8ad039eda22"
     conflicts_with "hazelcast-enterprise@5.4.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
+    conflicts_with "hazelcast-enterprise@5.3.5", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.3.2", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.3.1", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.3.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
