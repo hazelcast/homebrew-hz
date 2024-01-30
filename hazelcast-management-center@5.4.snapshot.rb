577fb9f38648dd3fa289389a8968d116cfac856e
devOpsHazelcast <devops@hazelcast.com>
2024-01-30 15:29:33 +0000

    Hazelcast Management Center 5.4-SNAPSHOT release

--- hazelcast-management-center@5.4.snapshot.rb
+++ hazelcast-management-center@5.4.snapshot.rb
@@ -3,7 +3,7 @@ class HazelcastManagementCenterAT54Snapshot < Formula
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "5.4-SNAPSHOT"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
-    sha256 "82c9fda4c00be61d1f8ad2b1adfeaa3067ada3f73d137ac19fad89a1107541b1"
+    sha256 "75e3c2dafd0b9a75d3a512df04d1ae535f5656714f6ce9ee0356911179ce7515"
     conflicts_with "hazelcast-management-center@5.3.snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.3.3", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.3.2", because: "you can install only a single hazelcast-management-center package"
