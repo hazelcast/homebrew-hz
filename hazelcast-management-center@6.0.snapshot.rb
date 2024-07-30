2daa405ed3d4e5ce975ff783b31a65f7a4e2c652
devOpsHazelcast <devops@hazelcast.com>
2024-07-30 09:19:08 +0000

    Hazelcast Management Center 6.0-SNAPSHOT release

--- hazelcast-management-center@6.0.snapshot.rb
+++ hazelcast-management-center@6.0.snapshot.rb
@@ -1,10 +1,9 @@
-class HazelcastManagementCenterSnapshot < Formula
+class HazelcastManagementCenterAT60Snapshot < Formula
     desc "Tool to run Hazelcast Management Center"
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "6.0-SNAPSHOT"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
     sha256 "e6ea677c3a8d52ef8881a3d53d15e57d2a61e9bab0753920ec3f440a173dd1ad"
-    conflicts_with "hazelcast-management-center@6.0.snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.5.snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.5.0", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.4.snapshot", because: "you can install only a single hazelcast-management-center package"
@@ -28,6 +27,7 @@ class HazelcastManagementCenterSnapshot < Formula
     conflicts_with "hazelcast-management-center-5.2", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-5.1", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-5.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center", because: "you can install only a single hazelcast-management-center package"
 
     depends_on "openjdk@21" => :recommended
