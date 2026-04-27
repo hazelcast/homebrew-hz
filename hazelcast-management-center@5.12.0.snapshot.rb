f35eb3e5bba915d124ef13a481209951c29fc8a6
devOpsHazelcast <devops@hazelcast.com>
2026-04-27 12:09:10 +0000

    Hazelcast Management Center 5.12.0-SNAPSHOT release

---
+++ hazelcast-management-center@5.12.0.snapshot.rb
@@ -0,0 +1,80 @@
+class HazelcastManagementCenterAT5120Snapshot < Formula
+    desc "Tool to run Hazelcast Management Center"
+    homepage "https://github.com/hazelcast/hazelcast-command-line"
+    version "5.12.0-SNAPSHOT"
+    url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
+    sha256 "405920ffbf51da5ec08c2f906c946627bce618fcd15e7ec31ca4d89f23c1bf13"
+    conflicts_with "hazelcast-management-center@6.0.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@6.0.0.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.9.1.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.9.0.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.9.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.8.1.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.8.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.7.2.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.7.1.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.7.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.7.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.6.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.6.2.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.6.1.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.6.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.3.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.2.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.2", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.1.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.5.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.4.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.4.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.4.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.4", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.3", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.2", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.1.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.3.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.2.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.2.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.2.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.11.0.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.10.0.snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.10.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.1.4", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.1.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center@5.0.4", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.9", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.8", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.7", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.6", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.5", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.4", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.3", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.2", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.10", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.1", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-5.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-snapshot", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center", because: "you can install only a single hazelcast-management-center package"
+
+    depends_on "openjdk@21" => :recommended
+
+    def install
+      libexec.install Dir["*"]
+      Dir["#{libexec}/bin/*mc*"].each do |path|
+        executable_name = File.basename(path)
+        if executable_name.end_with? ".bat"
+          next
+        end
+        (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
+      end
+      prefix.install_metafiles
+    end
+
+    def post_install
+      exec "echo Hazelcast Management Center has been installed."
+    end
+
+  end
