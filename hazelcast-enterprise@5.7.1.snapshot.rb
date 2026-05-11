e47a5110987eb51fcc4fb528b9a61fcc6cb85b5b
devOpsHazelcast <7523732+devOpsHazelcast@users.noreply.github.com>
2026-05-11 10:10:33 +0000

    [create-pull-request] automated change

--- hazelcast-enterprise@5.7.1.snapshot.rb
+++ hazelcast-enterprise@5.7.1.snapshot.rb
@@ -2,7 +2,7 @@ class HazelcastEnterpriseAT571Snapshot < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     url "https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/5.7.1-SNAPSHOT/hazelcast-enterprise-distribution-5.7.1-SNAPSHOT.tar.gz"
-    sha256 "6e8f14935f6e75d776f7d09318bfadbd1238063109ce7aede8ef1c5aed4ee4e9"
+    sha256 "6cffc8fc9e915f9dbab658eca00509539576db0f1b7ca184dc606b447b4cbdda"
     conflicts_with "hazelcast-enterprise@6.0.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.8.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.7.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
