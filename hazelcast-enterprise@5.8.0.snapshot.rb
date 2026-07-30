8e2cf2e817baaa815c0efcf38d8592c8997274dd
devOpsHazelcast <devops@hazelcast.com>
2026-07-30 03:11:07 +0200

    Hazelcast Homebrew 5.8.0-SNAPSHOT hazelcast-enterprise release (#231)
    Generated by
    https://github.com/hazelcast/hazelcast-packaging/actions/runs/30504814113/job/90752190058.
    
    Co-authored-by: devOpsHazelcast <7523732+devOpsHazelcast@users.noreply.github.com>

--- hazelcast-enterprise@5.8.0.snapshot.rb
+++ hazelcast-enterprise@5.8.0.snapshot.rb
@@ -2,7 +2,7 @@ class HazelcastEnterpriseAT580Snapshot < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     url "https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/5.8.0-SNAPSHOT/hazelcast-enterprise-distribution-5.8.0-SNAPSHOT.tar.gz"
-    sha256 "b4b0cd78340886588f526018c2189cbffea80f0f9ca41f9e9ee8fa9d69f2d0ff"
+    sha256 "d9101eb6aad52d297867b912a2738a102619c82142bc05cd2ef7d57f76c51787"
     conflicts_with "hazelcast-enterprise@6.0.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.7.2.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.7.1.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
